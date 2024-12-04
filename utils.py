2220a26b4126cfb2fc17d6bf46eac14f733499ce
DobakDavid <dobakdavid@gmail.com>
2024-12-04 23:54:58 +0100

    Added new functions to utils

--- utils.py
+++ utils.py
@@ -2,8 +2,14 @@
 File containing various utility functions for PyTorch model training.
 """ 
 import torch
+import os
+from PIL import Image
 
 from pathlib import Path
+import shutil
+
+
+
 
 def save_model(model: torch.nn.Module,
                target_dir: str,
@@ -34,3 +40,27 @@ def save_model(model: torch.nn.Module,
   print(f"[INFO] Saving model to: {model_save_path}")
   torch.save(obj=model.state_dict(),
              f=model_save_path)
+  
+def check_and_remove_corrupted_image(file_path):
+  """Checks the directory and all subdirectories for corrupted image files and removes them.
+
+  Args:
+    file_path: A target PyTorch model to save.
+  
+  Example usage:
+    check_and_remove_corrupted_image(model="../content/images")
+  """
+  try:
+    with Image.open(file_path) as img:
+      img.load()  # Check if the image is corrupted (Use load instead of verify, because verify sometimes can be inaccurate)
+    return False  # Image is not corrupted
+  except (IOError, SyntaxError) as e:
+    print(f"Removing corrupted image: {file_path} - {e}")
+    os.remove(file_path)  # Remove corrupted image file
+    return True  # Image was corrupted and removed
+
+def scan_and_clean_directory(directory):
+  for root, dirs, files in os.walk(directory):
+    for file in files:
+      file_path = os.path.join(root, file)
+      check_and_remove_corrupted_image(file_path)
