e251ff7217e2db5fd5e910522388a2fd05d1ff10
DobakDavid <dobakdavid@gmail.com>
2024-12-06 04:05:43 +0100

    writing main function

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 import torch
+import torchvision
 
+device = "cpu"
+
+# Main code
+def main():
+    device = "cpu"
+
+    model = torchvision.models.efficientnet_b3(weights='DEFAULT').to(device)
+
+    # Freeze the effnet layers
+    for param in model.features.parameters():
+        param.requires_grad = False
+
+    # Set the manual seeds
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+
+    # Get the length of class_names (one output unit for each class)
+    output_shape = 66
+
+    # Recreate the classifier layer and seed it to the target device
+    model.classifier = torch.nn.Sequential(
+        torch.nn.Dropout(p=0.2, inplace=True),
+        torch.nn.Linear(in_features=1536,
+                        out_features=output_shape, # same number of output units as our number of classes
+                        bias=True)).to(device)
+
+
+if __name__ == "__main__":
+    main()
