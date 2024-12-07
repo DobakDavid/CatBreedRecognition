16da4031cd1f647a37c4f4718b95442ec4e5c889
DobakDavid <dobakdavid@gmail.com>
2024-12-07 20:38:28 +0100

    added train_step, test_step, train functions

--- utils.py
+++ utils.py
@@ -2,7 +2,9 @@
 File containing various utility functions for PyTorch model training.
 """ 
 import torch
+from torch import nn
 import os
+import tqdm
 from PIL import Image
 
 from pathlib import Path
@@ -131,15 +133,138 @@ def make_predictions(model: torch.nn.Module,
   return torch.stack(pred_probs)
 
 def accuracy_fn(y_true, y_pred):
-    """Calculates accuracy between truth labels and predictions.
+  """Calculates accuracy between truth labels and predictions.
 
-    Args:
-        y_true (torch.Tensor): Truth labels for predictions.
-        y_pred (torch.Tensor): Predictions to be compared to predictions.
+  Args:
+    y_true (torch.Tensor): Truth labels for predictions.
+    y_pred (torch.Tensor): Predictions to be compared to predictions.
 
-    Returns:
-        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
-    """
-    correct = torch.eq(y_true, y_pred).sum().item()
-    acc = (correct / len(y_pred)) * 100
-    return acc     
+  Returns:
+    [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
+  """
+  correct = torch.eq(y_true, y_pred).sum().item()
+  acc = (correct / len(y_pred)) * 100
+  return acc 
+
+def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device: torch.device):
+  """ Functionizing training loop. """
+
+  # Initialize training loss and training accuracy
+  train_loss, train_acc = 0, 0
+
+  # Put model into training mode
+  model.train()
+  for batch, (X, y) in enumerate(data_loader):
+
+    # 0. Sending the data to device
+    X, y = X.to(device), y.to(device)
+
+    # 1. Forward pass
+    y_pred = model(X)
+
+    # 2. Calculate the loss and accuracy
+    loss = loss_fn(y_pred, y)
+    train_loss += loss
+    train_acc += accuracy_fn(y_true = y, y_pred = y_pred.argmax(dim = 1))
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backward
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    # Divide total train loss and acc by length of train dataloader
+  train_loss /= len(data_loader)
+  train_acc /= len(data_loader)
+  print(f"Train loss: {train_loss: .5f} | Train acc: {train_acc:.2f}")
+
+  return train_loss.item(), train_acc
+
+def test_step(model: torch.nn.Module,
+              data_loader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device: torch.device):
+  """ Functionizing test loop. """
+
+  # Initialize test loss and training accuracy
+  test_loss, test_acc = 0, 0
+  model.eval()
+
+  # Put model into inference mode
+  with torch.inference_mode():
+    for X_test, y_test in data_loader:
+
+      # 0. Sending the data to device
+      X_test, y_test = X_test.to(device), y_test.to(device)
+
+      # 1. Forward pass
+      test_pred = model(X_test)
+
+      # 2. Calculate the loss and accuracy
+      test_loss += loss_fn(test_pred, y_test)
+      test_acc += accuracy_fn(y_true = y_test, y_pred = test_pred.argmax(dim = 1))
+
+    # Divide total test loss by length of test dataloader
+    test_loss /= len(data_loader)
+
+    # Calculate the test acc average
+    test_acc /= len(data_loader)
+
+  # Return the results
+  return test_loss.item(), test_acc
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          accuracy_fn,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5):
+
+  # 1. Create empty results dictionary
+  results = {"model_name": [model.__class__.__name__],
+             "train_loss": [],
+             "train_acc": [],
+             "test_loss": [],
+             "test_acc": []
+             }
+
+  # 2. Loop through training and testing steps for a number of epochs
+  for epoch in tqdm(range(epochs)):
+    train_loss, train_acc = train_step(model = model,
+                                       data_loader = train_dataloader,
+                                       loss_fn = loss_fn,
+                                       optimizer = optimizer,
+                                       accuracy_fn = accuracy_fn,
+                                       device = "cpu")
+    test_loss, test_acc = test_step(model = model,
+                                    data_loader = test_dataloader,
+                                    loss_fn = loss_fn,
+                                    accuracy_fn = accuracy_fn,
+                                    device = "cpu")
+
+    # 3. Print out the values
+    print(f"Epoch: {epoch + 1} | "
+          f"train_loss: {train_loss: .4f} | "
+          f"train_acc: {train_acc: .4f} | "
+          f"test_loss: {test_loss: .4f} | "
+          f"test_acc: {test_acc: .4f}"
+          )
+
+    # 4. Update the results dictionary
+    results["train_loss"].append(train_loss.item() if isinstance(train_loss, torch.Tensor) else train_loss)
+    results["train_acc"].append(train_acc.item() if isinstance(train_acc, torch.Tensor) else train_acc)
+    results["test_loss"].append(test_loss.item() if isinstance(test_loss, torch.Tensor) else test_loss)
+    results["test_acc"].append(test_acc.item() if isinstance(test_acc, torch.Tensor) else test_acc)
+
+  # Return the results at the end of the epochs
+  return results
