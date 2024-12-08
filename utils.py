0c967f72562176c1c6f407c913591ee6c48afe5b
DobakDavid <dobakdavid@gmail.com>
2024-12-08 02:42:56 +0100

    rename and redocument misnamed function in utils.py

--- utils.py
+++ utils.py
@@ -43,95 +43,6 @@ def save_model(model: torch.nn.Module,
   print(f"[INFO] Saving model to: {model_save_path}")
   torch.save(obj=model.state_dict(),
              f=model_save_path)
-  
-def check_and_remove_corrupted_image(file_path):
-  """Opens a file to check if it is a corrupted image. If the file is a corrupted image, the function removes it.
-
-  Args:
-    file_path: A file path to open.
-
-  Returns:
-    Returns True if the file was a corrupted image file.
-
-  Example usage:
-    check_and_remove_corrupted_image(model="../content/images/Tulipe_148461.jpg")
-  """
-  try:
-    with Image.open(file_path) as img:
-      img.load()  # Check if the image is corrupted (Use load instead of verify, because verify sometimes can be inaccurate)
-    return False  # Image is not corrupted
-  except (IOError, SyntaxError) as e:
-    print(f"Removing corrupted image: {file_path} - {e}")
-    os.remove(file_path)  # Remove corrupted image file
-    return True  # Image was corrupted and removed
-
-def scan_and_clean_directory(directory):
-  """Walks through a directory and it's subdirectories iteratively, and removes all of the corrupted image files.
-
-  Args:
-    directory: A file path of a root directory to open.
-
-  Example usage:
-    scan_and_clean_directory(model="../content/images")
-  """
-  for root, dirs, files in os.walk(directory):
-    for file in files:
-      file_path = os.path.join(root, file)
-      check_and_remove_corrupted_image(file_path)
-
-def remove_single_image_classes(data_dir, treshold = 1):
-  """Walks through a directory and it's subdirectories iteratively, and removes all of the corrupted image files.
-
-  Args:
-    directory: A file path of a root directory to open.
-
-  Example usage:
-    scan_and_clean_directory(model="../content/images")
-  """
-  for class_name in os.listdir(data_dir):
-    class_path = os.path.join(data_dir, class_name)
-    if os.path.isdir(class_path):
-      num_images = len(os.listdir(class_path))
-      if num_images <= treshold:  # Check if class has only one image or less
-        shutil.rmtree(class_path)  # Remove the class folder
-        print(f"Removed class with less than {treshold + 1} image(s): {class_name}") 
-
-def make_predictions(model: torch.nn.Module,
-                     data: list,
-                     device: torch.device = "cpu"):
-  """Makes predictions for the data with the given model.
-
-  Args:
-    model: Trained PyTorch model.
-    data: Input data represented as list for the model, the shape has to compatible with model's input.
-    device: PyTorch device, the default is "cpu".
-
-  Returns:
-    Return the prediction probabilities for classes.
-
-  Example usage:
-    make_predictions(model=model_0,
-                     data=Tulipe_example,
-                     device=device)
-  """
-  pred_probs = []
-  model.eval()
-  with torch.inference_mode():
-    for sample in data:
-      # Prepare sample
-      sample = torch.unsqueeze(sample, dim=0).to(device) # Add an extra dimension and send to device
-
-      # Forward pass
-      pred_logit = model(sample)
-
-      # Get prediction probability
-      pred_prob = torch.softmax(pred_logit.squeeze(), dim = 0)
-
-      # Get pred_prob off from device to cpu
-      pred_probs.append(pred_prob.cpu())
-
-  # Stack the pred_probs to turn list into a tensor
-  return torch.stack(pred_probs)
 
 def accuracy_fn(y_true, y_pred):
   """Calculates accuracy between truth labels and predictions.
@@ -330,6 +241,97 @@ def train(model: torch.nn.Module,
   # Return the results at the end of the epochs
   return results
 
+def make_predictions(model: torch.nn.Module,
+                     data: list,
+                     device: torch.device = "cpu"):
+  """Makes predictions for the data with the given model.
+
+  Args:
+    model: Trained PyTorch model.
+    data: Input data represented as list for the model, the shape has to compatible with model's input.
+    device: PyTorch device, the default is "cpu".
+
+  Returns:
+    Return the prediction probabilities for classes.
+
+  Example usage:
+    make_predictions(model=model_0,
+                     data=Tulipe_example,
+                     device=device)
+  """
+  pred_probs = []
+  model.eval()
+  with torch.inference_mode():
+    for sample in data:
+      # Prepare sample
+      sample = torch.unsqueeze(sample, dim=0).to(device) # Add an extra dimension and send to device
+
+      # Forward pass
+      pred_logit = model(sample)
+
+      # Get prediction probability
+      pred_prob = torch.softmax(pred_logit.squeeze(), dim = 0)
+
+      # Get pred_prob off from device to cpu
+      pred_probs.append(pred_prob.cpu())
+
+  # Stack the pred_probs to turn list into a tensor
+  return torch.stack(pred_probs)
+  
+def check_and_remove_corrupted_image(file_path):
+  """Opens a file to check if it is a corrupted image. If the file is a corrupted image, the function removes it.
+
+  Args:
+    file_path: A file path to open.
+
+  Returns:
+    Returns True if the file was a corrupted image file.
+
+  Example usage:
+    check_and_remove_corrupted_image(model="../content/images/Tulipe_148461.jpg")
+  """
+  try:
+    with Image.open(file_path) as img:
+      img.load()  # Check if the image is corrupted (Use load instead of verify, because verify sometimes can be inaccurate)
+    return False  # Image is not corrupted
+  except (IOError, SyntaxError) as e:
+    print(f"Removing corrupted image: {file_path} - {e}")
+    os.remove(file_path)  # Remove corrupted image file
+    return True  # Image was corrupted and removed
+
+def scan_and_clean_directory(directory):
+  """Walks through a directory and it's subdirectories iteratively, and removes all of the corrupted image files.
+
+  Args:
+    directory: A file path of a root directory to open.
+
+  Example usage:
+    scan_and_clean_directory(model="../content/images")
+  """
+  for root, dirs, files in os.walk(directory):
+    for file in files:
+      file_path = os.path.join(root, file)
+      check_and_remove_corrupted_image(file_path)
+
+def remove_low_image_classes(data_dir, treshold = 1):
+  """Removes all of the image classes (subdirectories) in the given root directory.
+
+  Args:
+    data_dir: A file path of a root directory to open.
+    treshold: The maximum value of images on the removed subdirectories.
+
+  Example usage:
+    remove_low_image_classes(data_dir = "../content/images",
+                             treshold = 5)
+  """
+  for class_name in os.listdir(data_dir):
+    class_path = os.path.join(data_dir, class_name)
+    if os.path.isdir(class_path):
+      num_images = len(os.listdir(class_path))
+      if num_images <= treshold:  # Check if class has only one image or less
+        shutil.rmtree(class_path)  # Remove the class folder
+        print(f"Removed class with less than {treshold + 1} image(s): {class_name}") 
+
 def split_data(data_dir: Path,
               train_dir: Path = Path("train"),
               test_dir: Path = Path("test"),
@@ -375,3 +377,4 @@ def split_data(data_dir: Path,
             shutil.copy(image_file, train_dir / class_type.name / image_file.name)
           else:
             shutil.copy(image_file, test_dir / class_type.name / image_file.name)
+
